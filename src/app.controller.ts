c48692815cad8856fc81b0345d7476f90ec12d32
SuryaRajendr <surya.crajendran@gmail.com>
2024-10-20 16:24:21 +0530

    Setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello') // users/hello
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get() // users
+  findAll() {
+    return []
+  }
+
+  @Get(':id') // users/:id
+  findOne(@Param('id') id:String) {
+    return { id}
+  }
+
+  @Post() 
+  create(@Body() user: {}) {
+    return user
   }
 }
